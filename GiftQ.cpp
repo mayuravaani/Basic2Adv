c616f2b961570085202f3c7299ffef116220ebb9
mayuravaani <34495733+mayuravaani@users.noreply.github.com>
2021-09-25 12:01:25 +0530

    Update GiftQ.cpp

--- GiftQ.cpp
+++ GiftQ.cpp
@@ -1,62 +1,42 @@
 #include <iostream>
 using namespace std;
 
-
-int chkPossible(int a[], int b[] ,int j) {
-	int k =1;
-	if(a[j] == 1 || b[j] == 1 ) {
-		k=0	;
-	}	
-	return k;
-}
-
-void fillArray(int a[], int b[], int n) {
-	for(int i = 0; i < n; i++) {
-		a[i] = 0;
-		b[i] = 0;
-	}
-}
-
-string joinBeads(int x[], int red[], int blue[], int i, int j, string s) {
+string joinBeads(int x[],int i, int j, string s) {
 	if(x[i] == 1) {
-		if(chkPossible(red,blue, j) == 0)
+		if(s[j] == 'B')
 		{
 		  	return "Not Possible";
 		}
-		red[j] = 1;
+		
 		s= s.append("R");
 	} 
-	else if(x[i] == 2) 
+	else if(x[i] == 2) // blue
 	{
-		if(chkPossible(red,blue, j) == 0) 
+		if(s[j] == 'R') 
 		{
 		  	return "Not Possible";
 		}
-		blue[j] = 1;
+	
 		s = s.append("B");
 	}	
 	return s;	  			
 }
 
-int  construct(int n, int r, int a[], int b[], int x[]) {
+int  construct(int n, int r, int a[], int b[], int x[]) { 
 	string s = "";
-	int red[n];
-	int blue[n];
-	
-	fillArray(red, blue, n);
-    	
-	for(int i=0; i<r; i++) {
-		if(a[i] == 0) {
+
+	for(int i=0; i<r; i++) { 
+		if(a[i] == 0) { 
 			for(int j = a[i]; j <= b[i]; j++) 
 			{
-	  			s = joinBeads(x, red, blue, i, j, s);
+	  			s = joinBeads(x,i, j, s);
 	  		} 
 		} 
 		else 
 		{
-			for(int j = a[i]+1; j <= b[i]; j++) 
+			for(int j = a[i]+1; j <= b[i]; j++)
 			{
-	  			s = joinBeads(x, red, blue, i, j, s);
+	  			s = joinBeads(x, i, j, s);
 		  	} 
 		}
 	}
@@ -75,10 +55,22 @@ int main() {
   int a1[] = { 0, 1,0};
   int b1[] = { 1, 2,2};
   int x1[] = { 1, 1,2};
+  
+  int a2[] = {0,2,4,6,7,8,9,10};
+  int b2[] = {2,4,6,7,8,9,10,13};
+  int x2[] = {1,2,1,2,1,2,1,2};
+  
+  int a3[] = {0,2,4,6,7,6,9,10};
+  int b3[] = {2,4,6,7,8,9,10,13};
+  int x3[] = {1,2,1,2,1,1,1,2};
 
-  construct(4, 2, a, b, x);
+  //construct(4, 2, a, b, x);
   cout << endl;
-  construct(3, 3, a1, b1, x1);
+  //construct(3, 3, a1, b1, x1);
+  cout << endl;
+  //construct(14, 8, a2, b2, x2);
+  construct(14, 8, a3, b3, x3);
+
 
   return 1;
 }
